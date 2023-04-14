7913aebaf1a6abd9db3bd5c66e34d4cf8e88dc46
Mielesgames <emieltlam11@gmail.com>
2023-04-14 11:12:15 +0200

    webscraping almost complete

--- webScrape.py
+++ webScrape.py
@@ -37,4 +37,44 @@ def GetAnswer(URL):
             Text = textLabel.getText()
             print(f"Answer: {Text}\n")
 
-GetAnswer("https://stackoverflow.com/questions/41342609/the-difference-between-comparison-to-np-nan-and-isnull")
+#GetAnswer("https://stackoverflow.com/questions/41342609/the-difference-between-comparison-to-np-nan-and-isnull")
+def inputQuestion(question = ""):
+    question = question.replace(" ", "+").replace("?", "%3F")
+    print(question)
+    print(f"https://www.google.com/search?client=opera-gx&q={question}&sourceid=opera&ie=UTF-8&oe=UTF-8")
+    page = requests.get(f"https://www.google.com/search?client=opera-gx&q={question}&sourceid=opera&ie=UTF-8&oe=UTF-8")
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find(id="search")
+    #print(results.prettify())
+    links = results.find_all("a") #---------------------------------------------------------------------------------------------------------
+    clean_links = [link.text for link in links if link.text]
+    status = None
+    keywords = ["https://stackoverflow.com"]
+    for keyword in keywords:
+        for link in clean_links:
+            if keyword in link:
+                status = "ok"
+                GetAnswer(link)
+                break
+    
+    # for link in links:
+        
+    #     checkMark = job_element.find("svg", class_="svg-icon iconCheckmarkLg")
+        # print(title_element.text.strip())
+        # print(company_element.text.strip())
+        # print(location_element.text.strip())
+        # print()
+        #print(python_jobs)
+    # python_job_elements = [
+    #     p_element for p_element in clea
+    # ]
+    # for job_element in python_job_elements:
+    #     # -- snip -- #
+    #     labels = job_element.find_all("a")
+    #     for textLabel in labels:
+    #         link = textLabel["href"]
+    #         print(link)
+    #         GetAnswer(link)
+#"https://www.google.com/search?client=opera-gx&q="{input}"&sourceid=opera&ie=UTF-8&oe=UTF-8"
+inputQuestion(input("yes: "))
